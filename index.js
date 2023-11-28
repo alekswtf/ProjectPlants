a84385fb1686777aede81998c0242cf917da511a
alekswtf <alekseywtfg@gmail.com>
2023-11-28 20:10:57 +0300

    first

---
+++ index.js
@@ -0,0 +1,34 @@
+let spoiler = document.querySelectorAll(".site");
+let activeIndex = null;
+console.log(spoiler);
+
+
+spoiler.forEach((el, index) => {
+    el.addEventListener("click", () => {
+        activeIndex = index;
+        
+        spoiler[activeIndex].calssList.toggle("active");
+    });
+});
+
+
+
+
+
+let label = document.querySelector(".dropdown-label");
+let options = document.querySelector(".dropdown-options");
+let option =document.querySelector(".option");
+let labelValue = document.querySelector(".contacts-span");
+
+
+option.forEach((el) => {
+    el.addEventListener("click", () => {
+        labelValue.innerHTML = el.innerHTML;
+        options.classList.remove("open");
+    });
+});
+
+label.addEventListener("click", () => {
+    options.classList.toggle("open");
+
+});
