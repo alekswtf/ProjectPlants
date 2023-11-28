1f8babd5030fc28a463e9f76dccbe15c781ace40
alekswtf <alekseywtfg@gmail.com>
2023-11-28 23:57:28 +0300

    add smart button

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-let spoiler = document.querySelectorAll(".site");
+/* let spoiler = document.querySelectorAll(".site");
 let activeIndex = null;
 console.log(spoiler);
 
@@ -9,7 +9,7 @@ spoiler.forEach((el, index) => {
         
         spoiler[activeIndex].calssList.toggle("active");
     });
-});
+}); */
 
 
 
@@ -17,16 +17,15 @@ spoiler.forEach((el, index) => {
 
 let label = document.querySelector(".dropdown-label");
 let options = document.querySelector(".dropdown-options");
-let option =document.querySelector(".option");
+let option = document.querySelector(".option");
 let labelValue = document.querySelector(".contacts-span");
 
-
-option.forEach((el) => {
+/* option.forEach((el) => {
     el.addEventListener("click", () => {
         labelValue.innerHTML = el.innerHTML;
         options.classList.remove("open");
     });
-});
+}); */
 
 label.addEventListener("click", () => {
     options.classList.toggle("open");
